3c91db28fe1f14a6123e55cbfb2b5ed172882b1f
Stephan Binner <binner@kde.org>
2004-04-14 11:50:31 +0000

    More KStdGuiItem
    svn path=/trunk/kdeutils/kwallet/; revision=303588

--- kwalletpopup.cpp
+++ kwalletpopup.cpp
@@ -60,7 +60,7 @@ KWalletPopup::KWalletPopup(const QString& wallet, QWidget *parent, const char *n
 		insertItem(i18n("Disconnec&t"), pm);
 	}
 
-	act = new KAction(KStdGuiItem::close().text(), 0, 0, this,
+	act = KStdAction::close( this,
 			SLOT(closeWallet()), ac, "wallet_close");
 	// FIXME: let's track this inside the manager so we don't need a dcop
 	//        roundtrip here.
