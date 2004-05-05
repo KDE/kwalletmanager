bc79a3fe0510a9a3451a710e8f29d3831fbcc950
George Staikos <staikos@kde.org>
2004-05-05 17:46:30 +0000

    unbreak more of this
    svn path=/trunk/kdeutils/kwallet/; revision=308880

--- konfigurator/konfigurator.cpp
+++ konfigurator/konfigurator.cpp
@@ -55,6 +55,7 @@ KWalletConfig::KWalletConfig(QWidget *parent, const char *name, const QStringLis
 	connect(_wcw->_closeIdle, SIGNAL(clicked()), this, SLOT(configChanged()));
 	connect(_wcw->_openPrompt, SIGNAL(clicked()), this, SLOT(configChanged()));
 	connect(_wcw->_screensaverLock, SIGNAL(clicked()), this, SLOT(configChanged()));
+	connect(_wcw->_localWalletSelected, SIGNAL(clicked()), this, SLOT(configChanged()));
 	connect(_wcw->_idleTime, SIGNAL(valueChanged(int)), this, SLOT(configChanged()));
 	connect(_wcw->_launch, SIGNAL(clicked()), this, SLOT(launchManager()));
 	connect(_wcw->_newWallet, SIGNAL(clicked()), this, SLOT(newNetworkWallet()));
