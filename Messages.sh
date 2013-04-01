16f9ef4dc123ae9a095b30ad4ba4c3bb3b9fee93
Montel Laurent <montel@kde.org>
2013-04-01 12:19:20 +0200

    moved

--- Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-$EXTRACTRC `find . -name '*.ui' -or -name '*.rc'` >> rc.cpp || exit 11
-$XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/'` -o $podir/kwalletmanager.pot
-rm -f rc.cpp
