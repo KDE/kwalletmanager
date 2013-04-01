fbb55826f601fcc9f645b6936adab7f17652e743
Montel Laurent <montel@kde.org>
2013-04-01 12:16:31 +0200

    Fix extract messages

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-$EXTRACTRC *.rc *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/kwalletmanager.pot
+$EXTRACTRC `find . -name '*.ui' -or -name '*.rc'` >> rc.cpp || exit 11
+$XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/'` -o $podir/kwalletmanager.pot
+rm -f rc.cpp
