33d8269c736c67545f41612162d608113bab88a0
Laurent Montel <montel@kde.org>
2022-01-02 21:01:05 +0100

    It's enabled by default in qt6

--- src/manager/main.cpp
+++ src/manager/main.cpp
@@ -21,8 +21,10 @@
 
 int main(int argc, char **argv)
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+#endif
     QApplication a(argc, argv);
     KCrash::initialize();
     KLocalizedString::setApplicationDomain("kwalletmanager");
